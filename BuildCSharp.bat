1027a674302ec54630575eca37af14a6f813cddb
BCOM\tlesaunier <thibault.lesaunier@b-com.com>
2019-09-17 10:16:13 +0200

    Update

--- BuildCSharp.bat
+++ BuildCSharp.bat
@@ -1,6 +1,6 @@
-mkdir "%REMAKENROOT%/SolARPipelineManager/0.5.2/CSharp"
-del /s "%REMAKENROOT%\SolARPipelineManager\0.5.2\CSharp\*.*"
-swig -csharp -namespace SolAR -c++ -fcompact -small -O -Iswig -I%REMAKENROOT%/xpcf/2.2.0/interfaces -I%REMAKENROOT%/SolARFramework/0.5.2/interfaces -DSWIG_CSHARP_NO_WSTRING_HELPER -outdir "%REMAKENROOT%/SolARPipelineManager/0.5.2/CSharp" -o "src/SolARPluginPipelineManager_wrap.cpp" interfaces/SolARPipelineManager.i
+mkdir "%REMAKENROOT%/SolARPipelineManager/0.6.0/CSharp"
+del /s "%REMAKENROOT%\SolARPipelineManager\0.6.0\CSharp\*.*"
+swig -csharp -namespace SolAR -c++ -fcompact -small -O -Iswig -I%REMAKENROOT%/xpcf/2.2.0/interfaces -I%REMAKENROOT%/SolARFramework/0.6.0/interfaces -DSWIG_CSHARP_NO_WSTRING_HELPER -outdir "%REMAKENROOT%/SolARPipelineManager/0.6.0/CSharp" -o "src/SolARPluginPipelineManager_wrap.cpp" interfaces/SolARPipelineManager.i
 
 echo off
 echo ------------------ sub Bat file completed -----------------------------
