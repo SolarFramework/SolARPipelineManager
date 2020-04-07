45f58ee0b1023ea0ad8516b4049c5368fd03c974
BCOM\jroyan <jerome.royan@b-com.com>
2020-04-07 18:22:11 +0200

    Move to xpcf 2.3.4

--- BuildCSharp.bat
+++ BuildCSharp.bat
@@ -3,7 +3,7 @@ SET compiler=win-cl-14.1
 mkdir "%REMAKEN_PKG_ROOT%/packages/SolARBuild/%compiler%/SolARPipelineManager/0.7.0/csharp"
 del /s "%REMAKEN_PKG_ROOT%\packages\SolARBuild\%compiler%\SolARPipelineManager\0.7.0\csharp\*.*"
 
-swig -csharp -namespace SolAR -c++ -fcompact -small -O -Iswig -I%REMAKEN_PKG_ROOT%/packages/%compiler%/xpcf/2.3.3/interfaces -I%REMAKEN_PKG_ROOT%/packages/SolARBuild/%compiler%/SolARFramework/0.7.0/interfaces -DXPCF_USE_BOOST -DSWIG_CSHARP_NO_WSTRING_HELPER -outdir "%REMAKEN_PKG_ROOT%/packages/SolARBuild/%compiler%/SolARPipelineManager/0.7.0/csharp" -o "src/SolARPluginPipelineManager_wrap.cpp" interfaces/SolARPipelineManager.i
+swig -csharp -namespace SolAR -c++ -fcompact -small -O -Iswig -I%REMAKEN_PKG_ROOT%/packages/%compiler%/xpcf/2.3.4/interfaces -I%REMAKEN_PKG_ROOT%/packages/SolARBuild/%compiler%/SolARFramework/0.7.0/interfaces -DXPCF_USE_BOOST -DSWIG_CSHARP_NO_WSTRING_HELPER -outdir "%REMAKEN_PKG_ROOT%/packages/SolARBuild/%compiler%/SolARPipelineManager/0.7.0/csharp" -o "src/SolARPluginPipelineManager_wrap.cpp" interfaces/SolARPipelineManager.i
 
 echo off
 echo ------------------ sub Bat file completed -----------------------------
--- BuildCSharp.sh
+++ BuildCSharp.sh
@@ -3,7 +3,7 @@ COMPILER="linux-gcc"
 mkdir -p "$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARPipelineManager/0.7.0/csharp"
 rm -r "$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARPipelineManager/0.7.0/csharp/*.*"
 
-swig -csharp -namespace SolAR -c++ -fcompact -small -O -Iswig -I$HOME/.remaken/packages/$COMPILER/xpcf/2.3.3/interfaces -I$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARFramework/0.7.0/interfaces -DXPCF_USE_BOOST -DSWIG_CSHARP_NO_WSTRING_HELPER -outdir "$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARPipelineManager/0.7.0/csharp" -o "src/SolARPluginPipelineManager_wrap.cpp" interfaces/SolARPipelineManager.i
+swig -csharp -namespace SolAR -c++ -fcompact -small -O -Iswig -I$HOME/.remaken/packages/$COMPILER/xpcf/2.3.4/interfaces -I$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARFramework/0.7.0/interfaces -DXPCF_USE_BOOST -DSWIG_CSHARP_NO_WSTRING_HELPER -outdir "$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARPipelineManager/0.7.0/csharp" -o "src/SolARPluginPipelineManager_wrap.cpp" interfaces/SolARPipelineManager.i
 
 echo off
 echo ------------------ sub Bat file completed -----------------------------
