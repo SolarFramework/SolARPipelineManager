965c14808ad4f8e858e677cee42d343cf816f16e
Jérôme Royan <jerome.royan@b-com.com>
2020-05-04 16:28:55 +0200

    Check if csharp interfaces already exists before suppression

--- BuildCSharp.sh
+++ BuildCSharp.sh
@@ -2,10 +2,16 @@ COMPILER="linux-gcc"
 SOLAR_VERSION=0.8.0
 XPCF_VERSION=2.3.4
 
-mkdir -p $HOME/.remaken/packages/SolARBuild/$COMPILER/SolARPipelineManager/$SOLAR_VERSION/csharp
-rm -r $HOME/.remaken/packages/SolARBuild/$COMPILER/SolARPipelineManager/$SOLAR_VERSION/csharp/*.*
+CSharpFolder=$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARPipelineManager/$SOLAR_VERSION/csharp
 
-swig -csharp -namespace SolAR -c++ -fcompact -small -O -Iswig -I$HOME/.remaken/packages/$COMPILER/xpcf/$XPCF_VERSION/interfaces -I$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARFramework/$SOLAR_VERSION/interfaces -DXPCF_USE_BOOST -DSWIG_CSHARP_NO_WSTRING_HELPER -outdir "$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARPipelineManager/$SOLAR_VERSION/csharp" -o "src/SolARPluginPipelineManager_wrap.cpp" interfaces/SolARPipelineManager.i
+mkdir -p $CSharpFolder
+if [ "$(ls -A $CSharpFolder)" ]; then
+    echo "Suppress csharp interfaces in $CSharpFolder/"
+    rm -r $CSharpFolder/*.*
+fi
+
+echo "Generate SolARPipelineManager csharp interfaces with SWIG"
+swig -csharp -namespace SolAR -c++ -fcompact -small -O -Iswig -I$HOME/.remaken/packages/$COMPILER/xpcf/$XPCF_VERISON/interfaces -I$HOME/.remaken/packages/SolARBuild/$COMPILER/SolARFramework/$SOLAR_VERSION/interfaces -DXPCF_USE_BOOST -DSWIG_CSHARP_NO_WSTRING_HELPER -outdir "$CSharpFolder" -o "src/SolARPluginPipelineManager_wrap.cpp" interfaces/SolARPipelineManager.i
 
 echo off
 echo ------------------ sub Bat file completed -----------------------------
